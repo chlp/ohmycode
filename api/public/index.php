01cca3af0f10c07e441329f0effc96394f48555f
Aleksei Rytikov <user.raa@gmail.com>
2023-11-13 16:44:39 +0100

    using codemirror

--- api/public/index.php
+++ api/public/index.php
@@ -2,24 +2,70 @@
 <html lang="en">
 
 <head>
+    <title>OhMyCode</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="/favicon.png">
-
-    <title>OhMyCode</title>
+    <link rel="icon" type="image/png" href="favicon.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.63.0/codemirror.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.63.0/codemirror.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.63.0/mode/javascript/javascript.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.63.0/mode/go/go.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.63.0/mode/sql/sql.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.63.0/mode/php/php.js"></script>
+    <style>
+        .CodeMirror {
+            border: 1px solid #666;
+            max-width: 1000px;
+            width: 50vw;
+            min-width: 500px;
+        }
+    </style>
 </head>
-
 <body>
 
 <?php
-require 'db.php';
-$conn = dbConn();
-$sql = "SELECT * FROM `sessions`";
-$result = mysqli_query($conn, $sql);
-while ($row = mysqli_fetch_assoc($result)) {
-    var_dump($row);
+$code = '';
+$lang = '';
+$executor = '';
+$executorCheckedAt = null;
+$updatedAt = null;
+
+$id = $_GET['session'] ?? null;
+if ($id !== null) {
+    $id = (string)$id;
+    require 'db.php';
+    $dbConn = dbConn();
+    $stmt = $dbConn->prepare("SELECT `code`, `lang`, `executor`, `executor_checked_at`, `updated_at` FROM `sessions` WHERE `id` = ?");
+    if (!$stmt) {
+        die('wrong stmt');
+    }
+    $stmt->bind_param('s', $id);
+    $stmt->execute();
+    $stmt->bind_result($code, $lang, $executor, $executorCheckedAt, $updatedAt);
+    $stmt->fetch();
+    $stmt->close();
 }
 ?>
-</body>
 
+
+<textarea id="code"><?= $code ?></textarea>
+<button onclick="importCode()">Import Code</button>
+
+<script>
+    let editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+        lineNumbers: true,
+        mode: "sql", // javascript, go, php, sql
+        matchBrackets: true,
+    });
+
+    function importCode() {
+        // Get the code from the CodeMirror editor
+        var code = editor.getValue();
+        console.log("Imported Code:", code);
+        // You can now use the 'code' variable as needed
+    }
+</script>
+
+
+</body>
 </html>
