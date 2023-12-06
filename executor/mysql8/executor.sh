610bce9533630832f8926283a119cc880569518b
Aleksei Rytikov <user.raa@gmail.com>
2023-12-06 01:36:50 +0100

    added mysql8 executor

---
+++ executor/mysql8/executor.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+exec docker-entrypoint.sh "$@" &
+sleep 15 # waiting for mysql to start
+export MYSQL_PWD=root_password
+
+while [ True ]; do
+    if [ ! -z "$(ls requests)" ]; then
+        for REQUEST in requests/*; do
+            ID=$(basename $REQUEST)
+            mysql -e "CREATE DATABASE $ID;"
+            mysql $ID < $REQUEST 1>results/$ID 2>&1
+            mysql -e "DROP DATABASE $ID;"
+            rm $REQUEST
+        done
+    fi
+    sleep 1
+done
