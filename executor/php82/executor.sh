fb21cf36522a69f0bac9365820a0777fffd303ec
Aleksei Rytikov <user.raa@gmail.com>
2024-01-20 01:27:51 +0100

    executor.sh: fix empty results

--- executor/php82/executor.sh
+++ executor/php82/executor.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+mkdir tmp
 while [ True ]; do
+    if [ ! -z "$(ls tmp)" ]; then
+      rm tmp/*
+    fi
     if [ ! -z "$(ls requests)" ]; then
         for REQUEST in requests/*; do
             ID=$(basename $REQUEST)
-            php $REQUEST 1>results/$ID 2>&1
-            rm $REQUEST
+            php $REQUEST 1>tmp/$ID 2>&1
+            mv tmp/$ID results/$ID
         done
     fi
     sleep 1
