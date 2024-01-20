9a93a7819a2ade93682876a2a58202ea7603c464
Aleksei Rytikov <user.raa@gmail.com>
2024-01-20 01:39:06 +0100

    executor.sh: added timeout

--- executor/php82/executor.sh
+++ executor/php82/executor.sh
@@ -8,8 +8,12 @@ while [ True ]; do
     if [ ! -z "$(ls requests)" ]; then
         for REQUEST in requests/*; do
             ID=$(basename $REQUEST)
-            php $REQUEST 1>tmp/$ID 2>&1
+            timeout 5 php $REQUEST 1>tmp/$ID 2>&1
+            if [ $? -eq 124 ]; then
+              echo -e "\n\n---------------------\nTimeout reached, aborting\n---------------------\n" >> tmp/$ID
+            fi
             mv tmp/$ID results/$ID
+            rm $REQUEST
         done
     fi
     sleep 1
