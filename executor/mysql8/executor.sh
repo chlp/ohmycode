3713ed0c9fc684f9d70af5dadc95f91429a94130
Aleksei Rytikov <user.raa@gmail.com>
2024-01-20 01:43:55 +0100

    executor.sh: timeout & fix empty results for mysql

--- executor/mysql8/executor.sh
+++ executor/mysql8/executor.sh
@@ -4,13 +4,21 @@ exec docker-entrypoint.sh "$@" &
 sleep 15 # waiting for mysql to start
 export MYSQL_PWD=root_password
 
+mkdir tmp
 while [ True ]; do
+    if [ ! -z "$(ls tmp)" ]; then
+      rm tmp/*
+    fi
     if [ ! -z "$(ls requests)" ]; then
         for REQUEST in requests/*; do
             ID=$(basename $REQUEST)
             mysql -e "CREATE DATABASE $ID;"
-            mysql $ID --table < $REQUEST 1>results/$ID 2>&1
+            timeout 5 mysql $ID --table < $REQUEST 1>tmp/$ID 2>&1
+            if [ $? -eq 124 ]; then
+              echo -e "\n\n---------------------\nTimeout reached, aborting\n---------------------\n" >> tmp/$ID
+            fi
             mysql -e "DROP DATABASE $ID;"
+            mv tmp/$ID results/$ID
             rm $REQUEST
         done
     fi
