156342a3ada5133d1932adf71e3cc34be26b0af3
Aleksei Rytikov <user.raa@gmail.com>
2024-01-25 15:30:25 +0100

    fix mysql executor

--- executor/mysql8/executor.sh
+++ executor/mysql8/executor.sh
@@ -12,12 +12,12 @@ while [ True ]; do
     if [ ! -z "$(ls requests)" ]; then
         for REQUEST in requests/*; do
             ID=$(basename $REQUEST)
-            mysql -e "CREATE DATABASE $ID;"
-            timeout 5 mysql $ID --table < $REQUEST 1>tmp/$ID 2>&1
+            mysql -e "CREATE DATABASE tmp_$ID;"
+            timeout 5 mysql tmp_$ID --table < $REQUEST 1>tmp/$ID 2>&1
             if [ $? -eq 124 ]; then
               echo -e "\n\n-------------------------\nTimeout reached, aborting\n-------------------------\n" >> tmp/$ID
             fi
-            mysql -e "DROP DATABASE $ID;"
+            mysql -e "DROP DATABASE tmp_$ID;"
             mv tmp/$ID results/$ID
             rm $REQUEST
         done
