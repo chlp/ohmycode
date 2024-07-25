95d66a66149aef799d6c224e783a2f7f5189bf7f
Aleksei Rytikov <user.raa@gmail.com>
2024-07-25 13:42:14 +0200

    runner: fix problems & more tmpfs

--- runner/mysql8/runner.sh
+++ runner/mysql8/runner.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-exec docker-entrypoint.sh "$@" &
-
-export MYSQL_PWD=
+export MYSQL_PWD=mysql8root
 
 while true; do
-    mysql -e "SHOW DATABASES;" 1>/dev/null 2>&1
+    mysql -u root -e "SHOW DATABASES;" 1>/dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo "Starting mysql8 runner"
         break
@@ -13,6 +11,8 @@ while true; do
     echo "Waiting for mysql8"
     sleep 2
 done
+
+cd /app
 
 mkdir -p tmp
 while [ True ]; do
@@ -23,12 +23,12 @@ while [ True ]; do
         for REQUEST in requests/*; do
             echo $REQUEST
             ID=$(basename $REQUEST)
-            mysql -e "CREATE DATABASE tmp_$ID;"
-            timeout 5 mysql tmp_$ID --table < $REQUEST 1>tmp/$ID 2>&1
+            mysql -u root -e "CREATE DATABASE tmp_$ID;"
+            timeout 5 mysql -u root tmp_$ID --table < $REQUEST 1>tmp/$ID 2>&1
             if [ $? -eq 124 ]; then
               echo -e "\n\n-------------------------\nTimeout reached, aborting\n-------------------------\n" >> tmp/$ID
             fi
-            mysql -e "DROP DATABASE tmp_$ID;"
+            mysql -u root -e "DROP DATABASE tmp_$ID;"
             mv tmp/$ID results/$ID
             rm $REQUEST
         done
