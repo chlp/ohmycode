5014684bb58b54dfac589f585870eb5ce1df440c
Aleksei Rytikov <user.raa@gmail.com>
2024-07-27 23:07:20 +0200

    runner: postgres result format

--- runner/postgres13/runner.sh
+++ runner/postgres13/runner.sh
@@ -13,7 +13,7 @@ while [ True ]; do
             echo $REQUEST
             ID=$(basename $REQUEST)
             psql -U user -d mydatabase -c "CREATE DATABASE tmp_$ID;" 1>/dev/null 2>&1
-            timeout 5 psql -U user -d tmp_$ID < $REQUEST 1>tmp/$ID 2>&1
+            timeout 5 psql -U user -d tmp_$ID -q -c "\pset format wrapped" -f $REQUEST 1>tmp/$ID 2>&1
             if [ $? -eq 124 ]; then
               echo -e "\n\n-------------------------\nTimeout reached, aborting\n-------------------------\n" >> tmp/$ID
             fi
