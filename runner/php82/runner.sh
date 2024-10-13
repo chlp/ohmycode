9fdece0dd6035f94362e762a43cb1b8f12cd673b
Aleksei Rytikov <user.raa@gmail.com>
2024-10-13 23:30:59 +0200

    runner: php run php as restricted_user

--- runner/php82/runner.sh
+++ runner/php82/runner.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+adduser --disabled-password restricted_user
+
 mkdir -p tmp
 while [ True ]; do
     if [ -n "$(ls tmp)" ]; then
@@ -9,7 +11,7 @@ while [ True ]; do
         for REQUEST in requests/*; do
             echo $REQUEST
             ID=$(basename $REQUEST)
-            timeout 5 php $REQUEST 1>tmp/$ID 2>&1
+            su -c "timeout 5 php $REQUEST 1>tmp/$ID 2>&1" restricted_user
             if [ $? -eq 124 ]; then
               echo -e "\n\n-------------------------\nTimeout reached, aborting\n-------------------------\n" >> tmp/$ID
             fi
