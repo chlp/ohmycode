6876c447af6c29685e71c579b9252627e7b8e13e
Aleksei Rytikov <user.raa@gmail.com>
2024-10-15 12:43:53 +0200

    runner: fix access rights pg

--- runner/postgres13/runner.sh
+++ runner/postgres13/runner.sh
@@ -2,6 +2,11 @@
 
 cd /app
 
+adduser --disabled-password restricted_user
+
+mkdir -p tmp
+chmod -R 700 tmp
+
 export PGPASSWORD=password
 
 while [ True ]; do
@@ -12,15 +17,16 @@ while [ True ]; do
         for REQUEST in requests/*; do
             echo $REQUEST
             ID=$(basename $REQUEST)
+            touch tmp/$ID
+            chmod 700 tmp/$ID
             psql -U user -d mydatabase -c "CREATE DATABASE tmp_$ID;" 1>/dev/null 2>&1
-            timeout 5 psql -U user -d tmp_$ID -q -c "\pset format wrapped" -f $REQUEST 1>tmp/$ID 2>&1
+            su -c "timeout 5 psql -U user -d tmp_$ID -q -c \"\pset format wrapped\" -f $REQUEST" restricted_user 1>>tmp/$ID 2>&1
             if [ $? -eq 124 ]; then
               echo -e "\n\n-------------------------\nTimeout reached, aborting\n-------------------------\n" >> tmp/$ID
             fi
             psql -U user -d mydatabase -c "DROP DATABASE tmp_$ID;" 1>/dev/null 2>&1
             rm $REQUEST
             mv tmp/$ID results/$ID
-            chmod 777 results/$ID
         done
     fi
     sleep 0.01
