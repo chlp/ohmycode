b9886bf89796b7f569e5c87a03fa97219f7523c4
Aleksei Rytikov <user.raa@gmail.com>
2025-12-29 01:30:13 +0100

    client: increase js version

--- api/internal/api/client/js/main.js
+++ api/internal/api/client/js/main.js
@@ -1,8 +1,8 @@
-import "./hello_world.js?v=2";
-import "./connect.js?v=2";
-import "./editor.js?v=2";
-import "./open_file.js?v=2";
-import "./run.js?v=2";
-import "./file_name.js?v=2";
-import "./copy_url.js?v=2";
-import "./sidebar.js?v=2";
+import "./hello_world.js?v=3";
+import "./connect.js?v=3";
+import "./editor.js?v=3";
+import "./open_file.js?v=3";
+import "./run.js?v=3";
+import "./file_name.js?v=3";
+import "./copy_url.js?v=3";
+import "./sidebar.js?v=3";
