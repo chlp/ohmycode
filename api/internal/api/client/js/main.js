f5c4d1ebfa217329b6606c1aa74d52996425c03a
Aleksei Rytikov <user.raa@gmail.com>
2025-12-29 01:49:06 +0100

    client: js cache control 2

--- api/internal/api/client/js/main.js
+++ api/internal/api/client/js/main.js
@@ -1,8 +1,8 @@
-import "./hello_world.js?v=9";
-import "./connect.js?v=9";
-import "./editor.js?v=9";
-import "./open_file.js?v=9";
-import "./run.js?v=9";
-import "./file_name.js?v=9";
-import "./copy_url.js?v=9";
-import "./sidebar.js?v=9";
+import "./hello_world.js";
+import "./connect.js";
+import "./editor.js";
+import "./open_file.js";
+import "./run.js";
+import "./file_name.js";
+import "./copy_url.js";
+import "./sidebar.js";
